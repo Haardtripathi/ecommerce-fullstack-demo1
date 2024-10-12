5652ff359525493198fd875bf0571360962d2436
unknown <haard.tripathi14@gmail.com>
2024-10-13 03:05:50 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow only your frontend
+    origin: 'https://ecommerce-fullstack-demo-1-frontend.onrender.com', // Allow only your frontend
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
