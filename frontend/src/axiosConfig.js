dfc5617efeefc13356939a4bed5dd3205a5c4536
unknown <haard.tripathi14@gmail.com>
2024-10-13 02:59:59 +0530

    render update

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://ecommerce-fullstack-demo-1.onrender.com",
+    baseURL: "https://ecommerce-fullstack-demo1-1.onrender.com",
 });
 
 instance.interceptors.request.use(
